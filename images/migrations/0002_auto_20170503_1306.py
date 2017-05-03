589c12632034b92eab724256cbd741eac67c96d3
samdasoxide <samdasoxide@gmail.com>
2017-05-03 17:26:41 +0300

    Adding sorl-thumbnail

---
+++ images/migrations/0002_auto_20170503_1306.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('images', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='image',
+            name='image',
+            field=models.ImageField(upload_to='images/%Y/%m/%d/'),
+        ),
+    ]
